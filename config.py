58d0be150edfadc3e9459fd1dbd0db508a9873b4
chunhan2915 <68458221+chunhan2915@users.noreply.github.com>
2022-08-30 10:45:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvdrhltrp1uq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanchunhan-employee"
+customregion = "us-east-1"
