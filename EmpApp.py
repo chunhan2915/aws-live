b2db806a2a2fa10d39fc4dc07e737f13ce5bd2c9
chunhan2915 <68458221+chunhan2915@users.noreply.github.com>
2022-09-28 09:18:58 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -598,5 +598,52 @@ def conv_curr(amount):
     locale.setlocale(locale.LC_ALL, 'ms_MY') 
     return locale.currency(amount)
 
+@app.route("/applyLeave",methods=['POST','GET'])
+def applyLeave():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    phone = request.form['phone']
+    position = request.form['position']
+    department = request.form['department']
+    salary = request.form['salary']
+    emp_image_file = request.files['emp_image_file']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, email, phone,position,department,salary))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('index.html', name=emp_name,alert=True,add=True)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
